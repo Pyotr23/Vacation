b5ce725df55ff22acba7b9d3ff93117ed87625bd
Pyotr23 <2chilavert3@mail.ru>
2019-03-17 19:10:36 +0300

    Add employee model.

---
+++ Backend/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public List<Vacation> Vacations { get; set; }
+    }
+}
