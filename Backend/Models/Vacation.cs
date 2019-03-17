0bd0ba5e61ab9b2aac1887f8187666fd608cf5b0
Pyotr23 <2chilavert3@mail.ru>
2019-03-17 18:59:49 +0300

    Add vacation model.

---
+++ Backend/Models/Vacation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Vacation
+    {
+        public int ProductId { get; set; }
+        public DateTime Start { get; set; }
+        public int Duration { get; set; }
+        public DateTime Finish => Start.AddDays(Duration);
+    }
+}
