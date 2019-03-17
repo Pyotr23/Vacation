81f58e0aa5e648e9818666fc833ff7272b195cc5
Pyotr23 <2chilavert3@mail.ru>
2019-03-17 20:45:33 +0300

    Create database. Change property Finish and define constructor of Vacation model.

---
+++ Backend/Migrations/20190317173548_Initial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vacations",
+                columns: table => new
+                {
+                    VacationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Start = table.Column<DateTime>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                    EmployeeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacations", x => x.VacationId);
+                    table.ForeignKey(
+                        name: "FK_Vacations_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacations_EmployeeId",
+                table: "Vacations",
+                column: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vacations");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- Backend/Models/Vacation.cs
+++ Backend/Models/Vacation.cs
@@ -10,9 +10,14 @@ namespace Backend.Models
         public int VacationId { get; set; }
         public DateTime Start { get; set; }
         public int Duration { get; set; }
-        public DateTime Finish => Start.AddDays(Duration);
+        public DateTime Finish { get; set; }
 
         public int EmployeeId { get; set; }
         public Employee Employee { get; set; }
+
+        public Vacation()
+        {
+            Finish = Start.AddDays(Duration);
+        }
     }
 }
