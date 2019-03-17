f3641b356b6f4442331a8ef43152d5634e67928b
Pyotr23 <2chilavert3@mail.ru>
2019-03-17 19:46:39 +0300

    Create EFEmployeeRepository.

---
+++ Backend/Models/EFEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class EFEmployeeRepository : IEmployeeRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFEmployeeRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Employee> Employees => context.Employees;
+    }
+}
