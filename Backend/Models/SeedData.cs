4a1093fc4d913aea642e30e9cad348496fb805ba
Pyotr23 <2chilavert3@mail.ru>
2019-03-18 20:05:49 +0300

    Change SeedData() a little

--- Backend/Models/SeedData.cs
+++ Backend/Models/SeedData.cs
@@ -14,23 +14,23 @@ namespace Backend.Models
         {
             ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
             context.Database.Migrate();
-            if (!context.Vacations.Any())
+            if (!context.Employees.Any())
             {
-                //context.Employees.AddRange(
-                //    new Employee { Name = "Pyotr", Color = "Green" },
-                //    new Employee { Name = "Sergey", Color = "Blue" },
-                //    new Employee { Name = "Vasiliy", Color = "Red" }
-                //    );
-                
-                //context.SaveChanges();
+                context.Employees.AddRange(
+                    new Employee { Name = "Pyotr", Color = "Green" },
+                    new Employee { Name = "Sergey", Color = "Blue" },
+                    new Employee { Name = "Vasiliy", Color = "Red" }
+                    );
+
+                context.SaveChanges();
 
                 context.Vacations.AddRange(
-                    new Vacation { Start = new DateTime(2019, 5, 6), Duration = 7, Employee = context.Employees.Skip(0).FirstOrDefault() },
-                    new Vacation { Start = new DateTime(2019, 3, 11), Duration = 7, Employee = context.Employees.Skip(0).FirstOrDefault() },
-                    new Vacation { Start = new DateTime(2019, 5, 1), Duration = 28, Employee = context.Employees.Skip(1).FirstOrDefault() },
-                    new Vacation { Start = new DateTime(2019, 4, 30), Duration = 7, Employee = context.Employees.Skip(2).FirstOrDefault() },
-                    new Vacation { Start = new DateTime(2019, 5, 12), Duration = 7, Employee = context.Employees.Skip(2).FirstOrDefault() },
-                    new Vacation { Start = new DateTime(2019, 5, 25), Duration = 7, Employee = context.Employees.Skip(2).FirstOrDefault() }
+                    new Vacation { Start = new DateTime(2019, 5, 6), Duration = 7, Employee = context.Employees.ElementAt(0) },
+                    new Vacation { Start = new DateTime(2019, 3, 11), Duration = 7, Employee = context.Employees.ElementAt(0) },
+                    new Vacation { Start = new DateTime(2019, 5, 1), Duration = 28, Employee = context.Employees.ElementAt(1) },
+                    new Vacation { Start = new DateTime(2019, 4, 30), Duration = 7, Employee = context.Employees.ElementAt(2) },
+                    new Vacation { Start = new DateTime(2019, 5, 12), Duration = 7, Employee = context.Employees.ElementAt(2) },
+                    new Vacation { Start = new DateTime(2019, 5, 25), Duration = 7, Employee = context.Employees.ElementAt(2) }
                     );
 
                 context.SaveChanges();
