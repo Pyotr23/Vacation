22b66f6d76fe376e8c7ec56d448d831b9795d6d6
Pyotr23 <2chilavert3@mail.ru>
2019-03-19 18:16:41 +0400

    Create property Employees.

--- FrontendWPF/MainViewModel.cs
+++ FrontendWPF/MainViewModel.cs
@@ -5,11 +5,35 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace FrontendWPF
 {
     public class MainViewModel
     {
         private ObservableCollection<Employee> employees;
+
+        public ObservableCollection<Employee> Employees
+        {
+            get => employees;
+            set
+            {
+                employees = value;
+                OnPropertyChanged(nameof(Employees));
+            }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
