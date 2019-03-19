17c347ba3aba1712ffbdc4eacbe84655c86a53cf
Pyotr23 <2chilavert3@mail.ru>
2019-03-19 14:43:26 +0400

    Change SeedData() again.

--- Backend/Models/SeedData.cs
+++ Backend/Models/SeedData.cs
@@ -25,12 +25,12 @@ namespace Backend.Models
                 context.SaveChanges();
 
                 context.Vacations.AddRange(
-                    new Vacation { Start = new DateTime(2019, 5, 6), Duration = 7, Employee = context.Employees.ElementAt(0) },
-                    new Vacation { Start = new DateTime(2019, 3, 11), Duration = 7, Employee = context.Employees.ElementAt(0) },
-                    new Vacation { Start = new DateTime(2019, 5, 1), Duration = 28, Employee = context.Employees.ElementAt(1) },
-                    new Vacation { Start = new DateTime(2019, 4, 30), Duration = 7, Employee = context.Employees.ElementAt(2) },
-                    new Vacation { Start = new DateTime(2019, 5, 12), Duration = 7, Employee = context.Employees.ElementAt(2) },
-                    new Vacation { Start = new DateTime(2019, 5, 25), Duration = 7, Employee = context.Employees.ElementAt(2) }
+                    new Vacation { Start = new DateTime(2019, 5, 6), Duration = 7, Employee = context.Employees.Skip(0).FirstOrDefault() },
+                    new Vacation { Start = new DateTime(2019, 3, 11), Duration = 7, Employee = context.Employees.Skip(0).FirstOrDefault() },
+                    new Vacation { Start = new DateTime(2019, 5, 1), Duration = 28, Employee = context.Employees.Skip(1).FirstOrDefault() },
+                    new Vacation { Start = new DateTime(2019, 4, 30), Duration = 7, Employee = context.Employees.Skip(2).FirstOrDefault() },
+                    new Vacation { Start = new DateTime(2019, 5, 12), Duration = 7, Employee = context.Employees.Skip(2).FirstOrDefault() },
+                    new Vacation { Start = new DateTime(2019, 5, 25), Duration = 7, Employee = context.Employees.Skip(2).FirstOrDefault() }
                     );
 
                 context.SaveChanges();
