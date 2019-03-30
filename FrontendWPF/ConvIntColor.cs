e48e3d0eebd67269db31b9c1284d029288f68f5e
Pyotr23 <2chilavert3@mail.ru>
2019-03-30 19:28:05 +0300

    Add combobox.ItemTemplate for Colors.

--- FrontendWPF/ConvIntColor.cs
+++ FrontendWPF/ConvIntColor.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
-using System.Windows.Media;
 using FrontendWPF.Models;
 //using System.Windows.Media;
 
@@ -14,14 +12,21 @@ namespace FrontendWPF
 {
     public class ConvIntColor : IValueConverter
     {
+        Dictionary<int, string> colorDict = new Dictionary<int, string>{
+            [-16777216] = "Чёрный",            
+            [-16744448] = "Зелёный",
+            [-16776961] = "Синий",
+            [-65536] = "Красный",
+            [-256] = "Жёлтый",
+            [-8388480] = "Фиолетовый",
+            [-23296] = "Оранжевый",
+            [-8355712] = "Серый",
+            [-16711936] = "Лайм",
+            [-16181] = "Розовый"
+        };
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            Dictionary<System.Drawing.Color, string> ColorNames = new Dictionary<Color, string>();
-            foreach (var color in typeof(Colors).GetProperties())
-            {
-                ColorNames[(System.Windows.Media.Color)color.GetValue(null)] = color.Name;
-            }
-            return ((IEnumerable<Models.Color>)value).Select(x => System.Drawing.Color.FromArgb(x.ColorNumber));
+        {            
+            return colorDict[((Color)value).ColorNumber];
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
